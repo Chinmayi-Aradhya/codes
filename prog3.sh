c247be31f4020a32f3df61c10fc6184ca219a3a5
Chinmayi-Aradhya <chinmayiaradhya03@gmail.com>
2025-01-03 15:59:17 +0530

    git

---
+++ prog3.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#START#
+echo "Enter directory name : "
+read dir
+if [ ! -d $dir ]
+then
+echo "Invalid directory"
+exit
+fi
+large=0
+for file in `find $dir -type f`
+do 
+size=`stat -c %s $file`
+echo "Size of $file is $size"
+if [ $size -gt $large ]
+then
+large=$size
+fi
+done
+echo "File with maximum size is $large"
+#END#
