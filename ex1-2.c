4cfefb4768fe918c1418547ea6efe0c2f0f96bff
Chinmayi-Aradhya <chinmayiaradhya03@gmail.com>
2025-01-19 21:31:05 +0530

    c

--- ex1-2.c
+++ ex1-2.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 void main()
 {
-printf("Hello World\7");
-printf("Hello World\n");
-printf("Hello World\t");
+printf("Hello, World!\n");
+printf("Hello, World!\t");
+printf("Hello, World!\7");
 }
